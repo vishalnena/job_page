77be5a67a85413f7a81d8c12e6b957ab2435ef5d
vishal nenawate <vishalnenawate@gmail.com>
2024-01-05 14:39:27 +0530

    job

---
+++ job.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="job.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Urbanist:wght@600&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="main-home">
+        <div class="home-top">
+            <div class="image-div-1">
+                <img src="images/We are Hiring.jpeg" alt="">
+            </div>
+            <div class="image-div">
+                <img src="images\head.jpeg" alt="">
+            </div>
+        </div>
+
+
+        <div class="hiring-div">
+            <!-- <span class="intern-txt">
+                <h1 class="title">INTERNSHIP</h1>
+            </span> -->
+            <br>
+            <br>
+            <br>
+            <div class="card-div">
+                <div class="intern-card">
+                    <p></p>
+
+                    <p class="title-one"><i class="fa-solid fa-helmet-safety"></i> &nbsp;&nbsp;PRODUCTION</p>
+                    <p class="title-two"><i class="fa-solid fa-briefcase"></i>&nbsp;&nbsp;Expreince: Entry Level | Exp</p>
+                    <p class="title-edu"><i class="fa-solid fa-user-tie"></i>&nbsp;&nbsp;Qualification: Diploma |
+                        Bachelor's </p>
+                    <p class="title-three">
+                        The production department is responsible for efficiently transforming raw materials into
+                        finished goods,
+                        overseeing manufacturing processes, and ensuring quality control to meet production targets.
+                    </p>
+                </div>
+                <div class="intern-card">
+                    <p class="title-one"><i class="fa-solid fa-industry"></i>&nbsp;&nbsp;Manufacturing</p>
+                    <p class="title-two"><i class="fa-solid fa-briefcase"></i>&nbsp;&nbsp;Expreince: Entry Level | Exp</p>
+                    <p class="title-edu"><i class="fa-solid fa-user-tie"></i>&nbsp;&nbsp;Qualification: Diploma |
+                        Bachelor's </p>
+                    <p class="title-three">
+                        The manufacturing department is responsible for efficiently transforming raw materials into
+                        finished products through meticulous planning, precise execution, and continuous process
+                        improvement.
+                    </p>
+                </div>
+                <div class="intern-card">
+                    <p class="title-one"><i class="fa-solid fa-magnifying-glass"></i>&nbsp;&nbsp; Quality Assurance </p>
+                    <p class="title-two"><i class="fa-solid fa-briefcase"></i>&nbsp;&nbsp;Expreince: Entry Level | Exp</p>
+                    <p class="title-edu"><i class="fa-solid fa-user-tie"></i>&nbsp;&nbsp;Qualification: Diploma |
+                        Bachelor's </p>
+                    <p class="title-three">
+                        The Quality Assurance department ensures adherence to established standards and procedures,
+                        guaranteeing the consistent delivery of high-quality products or services through rigorous
+                        testing, inspection, and continuous improvement initiatives.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+
+
+
+        <br>
+        <br>
+        <br>
+        <div class="hiring-div">
+            <!-- <span class="intern-txt">
+                <h1 class="title">FULLTIME JOB</h1>
+            </span> -->
+            <br>
+            <br>
+            <br>
+            <div class="card-div">
+                <div class="intern-card">
+                    <p class="title-one"><i class="fa-solid fa-bolt"></i> &nbsp;&nbsp;Electrician</p>
+                    <p class="title-two"><i class="fa-solid fa-briefcase"></i>&nbsp;&nbsp;Expreince: Entry Level | Exp</p>
+                    <p class="title-edu"><i class="fa-solid fa-user-tie"></i>&nbsp;&nbsp;Qualification: Diploma |
+                        Bachelor's </p>
+                    <p class="title-three">
+                        The electric department specializes in the generation, distribution, and maintenance of
+                        electrical power infrastructure, ensuring reliable and efficient electricity supply to
+                        residential, commercial, and industrial areas.
+                    </p>
+                </div>
+                <div class="intern-card">
+                    <p class="title-one"><i class="fa-solid fa-box"></i>&nbsp;&nbsp;Packaging</p>
+                    <p class="title-two"><i class="fa-solid fa-briefcase"></i>&nbsp;&nbsp;Expreince: Entry Level | Exp</p>
+                    <p class="title-edu"><i class="fa-solid fa-user-tie"></i>&nbsp;&nbsp;Qualification: High school |
+                        equivalent </p>
+                    <p class="title-three">Quality assurance is the systematic process of ensuring that products or
+                        services meet specified requirements and standards to guarantee customer satisfaction.
+                    </p>
+                </div>
+                <div class="intern-card">
+                    <p class="title-one"><i class="fa-solid fa-pen-nib"></i>&nbsp;&nbsp; Molding</p>
+                    <p class="title-two"><i class="fa-solid fa-briefcase"></i>&nbsp;&nbsp;Expreince: Entry Level | Exp</p>
+                    <p class="title-edu"><i class="fa-solid fa-user-tie"></i>&nbsp;&nbsp;Qualification: High school |
+                        equivalent </p>
+                    <p class="title-three">The molding department specializes in the fabrication of various products
+                        through the precision molding of materials, employing advanced techniques and machinery to
+                        create high-quality components for diverse industries.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <br>
+        <br>
+        <br>
+        <div class="heading">
+            <div class="campus-drive">
+
+                <div class="p-div">
+                    <p> ARE YOU LOOKING FOR CAMPUS DRIVE IN YOUR COLLAGE ?</p>
+                </div>
+                <ul>
+                    <li>100% job assistance for your candidates.</li>
+                    <li>Campus drive with varios multinational branded industry.</li>
+                    <li>Maximum shortlisted candidates.</li>
+                    <li> Accommodation and food will be arranging by selected candidates(to candidate cost).</li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+</body>
+
+</html>
